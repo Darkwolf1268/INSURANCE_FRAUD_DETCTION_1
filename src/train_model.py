2aad74ebb482a3b01a8f45c4367edb49398ae3be
Darkwolf1268 <avnigautam.84@gmail.com>
2026-03-11 18:02:24 +0530

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,9 +1,13 @@
 import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from preprocess import preprocess_data
 
-data = preprocess_data("../dataset/insurance_fraud.csv")
+from src.data.preprocess import preprocess_data
+from src.features.feature_engineering import create_features
+
+data = preprocess_data("data/raw/insurance_fraud.csv")
+
+data = create_features(data)
 
 X = data.drop("fraud_reported", axis=1)
 y = data["fraud_reported"]
@@ -14,6 +18,6 @@ model = RandomForestClassifier()
 
 model.fit(X_train, y_train)
 
-pickle.dump(model, open("../model/fraud_model.pkl", "wb"))
+pickle.dump(model, open("models/fraud_model.pkl", "wb"))
 
 print("Model trained and saved successfully")
