eb8cdb5c6a6dc57077726f3d01b4185af8f376d2
Darkwolf1268 <avnigautam.84@gmail.com>
2026-03-11 17:57:24 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request,jsonify
+import pickle
+
+app = Flask(__name__)
+
+model = pickle.load(open("model/fraud_model.pkl","rb"))
+
+@app.route("/")
+def home():
+    return "Insurance Fraud Detection API"
+
+@app.route("/predict",methods=["POST"])
+def predict():
+
+    data = request.json["features"]
+
+    prediction = model.predict([data])[0]
+
+    if prediction == 1:
+        result = "Fraud Claim"
+    else:
+        result = "Legitimate Claim"
+
+    return jsonify({"Prediction":result})
+
+if __name__=="__main__":
+    app.run(debug=True)
